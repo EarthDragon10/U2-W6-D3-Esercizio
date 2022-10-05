a34c684e804eeebda138e47699a78357c56116f2
EarthDragon10 <lagudadev@gmail.com>
2022-10-05 17:02:23 +0200

    add some rules js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+let boxImg = document.getElementById("img-box");
+
 function changeBgColor() {
 	document.querySelector("body").style.backgroundColor = "red";
 }
 
-function insertImg() {}
+function insertImg() {
+	boxImg.innerHTML += `<img class="my-5" src="https://picsum.photos/200/300" alt="immagine">`;
+}
